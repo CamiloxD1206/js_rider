fe0e2285e670fe5747f49345e08f8443f4e02417
Camilo Tacue <cetacue@misena.edu.co>
2023-07-21 18:21:17 -0500

    camilo

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// //primerafase------------------------------------------------
+// function datos(){
+// let meses = document.getElementById("meses");
+// let selectMes =meses.options[meses.selectedIndex].text;
+// //----------------------------------------------
+// let nombre=document.getElementById("nombre").value;
+// //-------------------------------------------------
+// let años = document.getElementById("año");
+// let selectaño =años.options[años.selectedIndex].text;
+// //-------------------------------------------------
+// let trimes=document.getElementById("trimestre").value;
+// //----------------------------------------------
+// let competencia=document.getElementById("competencia").value;
+// //-----------------------------------------------------------------------------------------
+// let radio = document.querySelector('input[name="ganador"]:checked').value;
+// if(radio=='gana') {
+//     let texto=document.querySelector('#texto').textContent=(`El aprediz ${nombre} aprobo con exito la competencia ${competencia} en el ${trimes} trimestre,durante el mes de ${selectMes} en el año ${selectaño}`);
+// } else if(radio=='pierde') {
+//     let texto=document.querySelector('#texto').textContent=(`El aprediz ${nombre} no aprobo con exito la competencia ${competencia} en el ${trimes} trimestre,durante el mes de ${selectMes} en el año ${selectaño}`);
+// }
+
+   
+// }
+//segundafase-------------------index_segunda_fase---------------------------------------------
+
+function dracula(nombre,meses,año,competencia,trimestre){
+    let radio = document.querySelector('input[name="ganador"]:checked').value;
+    if(radio=='aprobo'){
+
+   let texto2=document.querySelector('#texto').textContent=(`El aprendiz ${nombre} aprobo la competencia ${trimestre},en el ${competencia} trimestre durante el mes de ${meses} del año ${año}`);
+}else if(radio=='perdio'){
+    let texto2=document.querySelector('#texto').textContent=(`El aprendiz ${nombre} perdio la competencia ${trimestre},en el ${competencia} trimestre durante el mes de ${meses} del año ${año}`);
+}
+}
+//tercerafase---------------index_segunda_fase-------------------------------------------------
+// function dracula(nombre,meses,año,competencia,trimestre){
+//     let array = [];
+//     array.push(nombre,meses,año,competencia,trimestre);
+//     console.log()
+// };
+
